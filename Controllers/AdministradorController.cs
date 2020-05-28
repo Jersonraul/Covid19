b3cf4c766ce6357d800a976bc8aa57073c3fc662
Jersonraul <63474008+Jersonraul@users.noreply.github.com>
2020-05-27 21:41:38 -0500

    listado api

--- Controllers/AdministradorController.cs
+++ Controllers/AdministradorController.cs
@@ -1,10 +1,13 @@
 ﻿using APPCOVID.Models;
+using Newtonsoft.Json;
 //using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Text.Json;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace APPCOVID.Controllers
 {
@@ -21,18 +24,38 @@ namespace APPCOVID.Controllers
             request.Wait();
             var response = request.Result;
 
-            if (response.IsSuccessStatusCode)
-            {
                 var resultString = response.Content.ReadAsStringAsync().Result;
-                Debug.WriteLine("Debug");
-                Debug.WriteLine(resultString);
-                SintomaResponse respuestaJSON = JsonSerializer.Deserialize<SintomaResponse>(resultString);
-                return View(respuestaJSON.ToString());
-            }
-            return null;
-            //return View(new List<Administrador>());
-        }
+                var listado = JsonConvert.DeserializeObject<SintomaResponse>(resultString);
 
+            List<Sintoma> listaSintoma = new List<Sintoma>();
+            foreach (var item in listado.sintomas)
+            {
+                Sintoma obj = new Sintoma()
+                {
+                    symptom_id = item.symptom_id,
+                    symptom_description = item.symptom_description
+                };
+                listaSintoma.Add(obj);
+            }
+
+            ViewBag.sintomass = listaSintoma;
+
+               /* for (var i= 0;i<listado.sintomas.Count;i++)
+                {
+                    Console.WriteLine(listado);
+                }
+
+            ViewBag.sintomasa = listado;*/
+
+                return View();
+
+                
+
+            
+
+           
+        }
+       
        
 
     }
--- Models/Sintoma.cs
+++ Models/Sintoma.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +8,16 @@ namespace APPCOVID.Models
 {
     public class Sintoma
     {
+        [JsonProperty("symptom_id")]
         public int symptom_id { get; set; }
+        [JsonProperty("symptom_description")]
         public String symptom_description { get; set; }
+    }
+
+         public class SintomaResponse
+    {
+        //private List<Sintoma> response { get; set; }
+        [JsonProperty("sintomas")]
+        public List<Sintoma> sintomas { get; set; }
     }
 }
